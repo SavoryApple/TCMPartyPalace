a0a1bc990ce600ec4a42e23d3b7b621f5b659ae4
Seann <seannavery@gmail.com>
2025-08-23 18:34:37 -0700

    added DOckerfile and fly.toml

--- backend/server.js
+++ backend/server.js
@@ -43,5 +43,5 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
